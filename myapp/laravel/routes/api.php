7659b5606d3c5a06652c4acbf99f66a75181f829
akiyoshi-takazawa <takazawa@arsaga.jp>
2021-03-01 22:58:43 +0900

    create JWT-auth

--- myapp/laravel/routes/api.php
+++ myapp/laravel/routes/api.php
@@ -13,6 +13,25 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', 'AuthController@login');
 });
+
+Route::group(['prefix' => 'auth', 'middleware' => 'auth:api'], function () {
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
+
+//
+//Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+//    Route::post('login', 'AuthController@login');
+//    Route::post('logout', 'AuthController@logout');
+//    Route::post('refresh', 'AuthController@refresh');
+//    Route::post('me', 'AuthController@me');
+//});
+//
+////Route::middleware('auth:api')->get('/user', function (Request $request) {
+////    return $request->user();
+////});
